a002c1edc39ade4c8adfb8fe0fce217bf174d2c6
SJCaldwell <shane.caldwell12@ncf.edu>
2020-06-27 12:05:09 -0500

    Add specific target for AI
    Remove EastWall so AI can fall
    
    Respawn target indefinitely

---
+++ Assets/Scripts/PlayerAgent.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+public class PlayerAgent : Agent
+{
+    private Rigidbody rb;
+    public Transform Target;
+    public float speed = 10;
+
+    void Start()
+    {
+       rb = GetComponent<Rigidbody>(); 
+    }
+
+    public override void OnEpisodeBegin(){
+        if (this.transform.localPosition.y < 0)
+        {
+            // If the Agent fell, zero its momentum
+            this.rb.angularVelocity = Vector3.zero;
+            this.rb.velocity = Vector3.zero;
+            this.transform.localPosition = new Vector3( 0, 0.5f, 0);
+        }
+
+        // Move the target to a new spot
+        Target.localPosition = new Vector3(Random.value * 8 - 4,
+                                           0.5f,
+                                           Random.value * 8 - 4);
+    }
+
+    public override void CollectObservations(VectorSensor sensor){
+        //Target and agent positions
+        sensor.AddObservation(Target.localPosition);
+        sensor.AddObservation(this.transform.localPosition);
+
+        // Agent velocity
+        sensor.AddObservation(rb.velocity.x);
+        sensor.AddObservation(rb.velocity.z);
+
+    }
+
+    public override void OnActionReceived(float[] vectorAction){
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = vectorAction[0];
+        controlSignal.z = vectorAction[1];
+        rb.AddForce(controlSignal * speed);
+
+        float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
+
+        if (distanceToTarget < 1.42f){
+            SetReward(1.0f);
+            EndEpisode();
+        }
+
+        // Fell of Platform
+        if (this.transform.localPosition.y < 0){
+            EndEpisode();
+        }
+    }
+
+    public override void Heuristic(float[] actionsOut)
+{
+    actionsOut[0] = Input.GetAxis("Horizontal");
+    actionsOut[1] = Input.GetAxis("Vertical");
+}
+
+
+}
